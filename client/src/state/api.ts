2a36282536ccac4a526fcb416585cd269bc10801
SAIRITHVIK18 <SNIPERSHOTX69@GMAIL.COM>
2023-12-22 19:24:25 +0530

    first commit

---
+++ client/src/state/api.ts
@@ -0,0 +1,38 @@
+//create Api function is used to fetch data from backend for KPIs,products and transactions
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  GetKpisResponse,
+  GetProductsResponse,
+  GetTransactionsResponse,
+} from "./types";
+
+
+//fetchs data from base url obtained from .env
+//reducerpath stores the in redux stores
+//tagtypes are unique tags for different data
+//endpoints which accepts requests and send back responses
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  reducerPath: "main",
+  tagTypes: ["Kpis", "Products", "Transactions"],
+  endpoints: (build) => ({
+    getKpis: build.query<Array<GetKpisResponse>, void>({
+      query: () => "kpi/kpis/",
+      providesTags: ["Kpis"],
+    }),
+    getProducts: build.query<Array<GetProductsResponse>, void>({
+      query: () => "product/products/",
+      providesTags: ["Products"],
+    }),
+    getTransactions: build.query<Array<GetTransactionsResponse>, void>({
+      query: () => "transaction/transactions/",
+      providesTags: ["Transactions"],
+    }),
+  }),
+});
+
+export const { useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery } =
+  api;
+
+
+//this api.ts is used for fetching data and used for retrieves data from redux toolkit query
